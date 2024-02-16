4c4cff7160dfcaed02f705a8b2e060ef2ff17ce8
narul-shxkeen <97353543+narul-shxkeen@users.noreply.github.com>
2024-02-16 17:34:11 +0530

    finished connect page

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-$(".one").click(function () {
+$("#one").click(function () {
     $(this).css("display","none");
     $(".op").css("display","block");
     $(".productName").text("Select Product")
 });
+$(".others").click(function (){
+    $(".item_").css("display","none");
+    $(".otherText").css("display","block");
+})
